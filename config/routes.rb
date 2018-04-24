1feaf38ef03c5e3cf23e332541ea9f13f45b414c
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-24 19:59:48 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
+
+  resources :users do
+    resources :trips, only: [:show, :new, :edit]
+  end
+
+  resources :countries
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post 'logout' => 'sessions#destroy'
+
 end
