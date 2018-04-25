1e26e50522d8a234aa40058fe7567d2c36998809
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-25 14:25:30 -0400

    log in form and sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user_id
+        redirect_to user_path(@user)
+      else
+        redirect_to '/login'
+      end
+    end
   end
 
   def destroy
   end
 
-end 
+end
