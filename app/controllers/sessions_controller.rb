e0412ff519cb0040234bd904ce6496bb5562e540
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-25 16:41:13 -0400

    building out signup form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,19 @@ class SessionsController < ApplicationController
 
   def create
     if @user = User.find_by(email: params[:email])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-      else
-        flash[:notice] = "Invalid e-mail or password."
-        redirect_to '/login'
-      end
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          flash[:notice] = "Invalid e-mail or password."
+          redirect_to '/login'
+        end
+    else
+      flash[:notice] = "We cannot find an account with that e-mail."
+      redirect_to '/login'
     end
   end
+
 
   def destroy
     session.delete :user_id
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login
-  skip_before_action :require_login, only: [:new]
+  skip_before_action :require_login, only: [:new, :create]
 
 
   def index
@@ -15,5 +15,19 @@ end
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :favorite_country)
+  end
 
 end
