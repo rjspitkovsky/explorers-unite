1f7fd1b9b665e5469a4ae822eb332634668a8a34
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-28 18:40:21 -0400

    added class methods to filter by year

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,22 @@
 class TripsController < ApplicationController
 
   def index
-    @trips = Trip.all
+    if !params[:year].blank?
+      if params[:year] == "pre-2000"
+        @trips = Trip.twentieth_century
+      elsif
+        params[:year] == "2000-2009"
+        @trips = Trip.aughts
+      elsif
+        params[:year] == "2010-2015"
+        @trips = Trip.first_half_of_teens
+      elsif
+        params[:year] == "2016-present"
+        @trips = Trip.second_half_of_teens
+      else
+        @trips = Trip.all
+      end
+    end 
   end
 
   def new
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,13 @@ class Trip < ApplicationRecord
   belongs_to :country
   validates :comment, length: {maximum: 100}
   validates :year, length: {is: 4}
+  validate :future_trip?
+
+    def future_trip?
+      if year > Time.new.year
+        errors.add(:year, "cannot be in the future.")
+      end
+    end
 
     def country_name=(name)
       self.country = Country.find_or_create_by(name: name)
@@ -10,5 +17,21 @@ class Trip < ApplicationRecord
 
     def country_name
       self.country.name if self.country
+    end
+
+    def self.twentieth_century
+      where('year <= ?', 2000)
+    end
+
+    def self.aughts
+      where('year > ? AND year < ?', 1999, 2010)
+    end
+
+    def self.first_half_of_teens
+      where('year > ? AND year < ?', 2009, 2016)
+    end
+
+    def self.second_half_of_teens
+      where('year > ? AND year < ?', 2015, 2019)
     end
 end
