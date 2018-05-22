2e59118bd46f7a69d75da23d6cee52b2be7ce2d9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-22 18:27:55 -0400

    building out script for getting user trip information in users#show page

--- app/assets/javascripts/loadTrips.js
+++ app/assets/javascripts/loadTrips.js
@@ -1,3 +1,26 @@
+// $(document).ready(function() {
+//   loadTrips()
+// })
+//
+//
+// function loadTrips() {
+//   // $('#load-trips').on('click', function(e) {
+//   //   let userId = $(e.target).attr('user-id')
+//   //   fetchTrips(userId)
+//   // })
+//   let userId = $(e.target).attr('user-id')
+//
+// }
+//
+// function fetchTrips(userId) {
+//   $.get('/users/' + userId + '/trips.json', function(data) {
+//     alert(data)
+//   })
+// }
+
 function loadTrips() {
-  $.get('/users/')
-}
+  let userId = parseInt($(".js-load-trips").attr("data-user-id"))
+  $.get(`/users/${userId}/trips`, function(data) {
+    alert(data[0]["comment"])
+  })
+  }
