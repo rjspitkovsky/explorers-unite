e5eeb422f830d86667aaeffde0a39e4e68da349b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-23 17:52:49 -0400

    building out fetchNextTripId prototype method

--- app/assets/javascripts/loadTrips.js
+++ app/assets/javascripts/loadTrips.js
@@ -3,6 +3,11 @@ function UserTrips(data){
   this.trips = data
 }
 
+UserTrips.prototype.findNextTripId = function(userId, tripId) {
+    return 23
+  }
+
+
 
 function loadTrips() {
   // $("#new-trip").empty()
@@ -22,3 +27,22 @@ function loadTrips() {
     // alert(data[0]["comment"])
   })
   }
+
+  function fetchNextTrip() {
+  let tripId = parseInt($(".js-next-trip").attr("data-trip-id"));
+  let userId = parseInt($(".js-next-trip").attr("data-user-id"))
+
+  $.get(`/users/${userId}/trips`, function(data) {
+    let user = new UserTrips(data)
+    let nextTripId = user.findNextTripId(userId, tripId)
+
+  // CODE WHERE findNextTripId(userId, tripId) function will go
+  $.get(`/users/${userId}/trips/${nextTripId}.json`, function(data) {
+    $(".next-trip-id").text(`Explorers Unite Trip ID: ${data["id"]}`);
+    $(".next-trip-comment").text(`Comment: ${data["comment"]}`);
+    $(".js-next-trip").attr("data-trip-id", data["id"]);
+    $(".next-trip-year").text(`Year Visited: ${data["year"]}`);
+    $(".next-trip-recommend").text(`Recommended? ${data["recommend?"]}`);
+  })
+  })
+}
